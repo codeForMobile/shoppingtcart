3a949b8c051a3a8d37ef085bed58e65c2f178355
codeForMobile <code.mobile@protonmail.com>
2021-03-22 22:54:18 +0200

    favorite working with providers

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,33 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
 import '../screens/product_detail_screen.dart';
+import '../providers/product.dart';
 
 class ProductItem extends StatelessWidget {
-  final String id;
+/*   final String id;
   final String title;
   final String imageUrl;
 
-  ProductItem(this.id, this.title, this.imageUrl);
-
+  ProductItem(this.id, this.title, this.imageUrl); */
   @override
   Widget build(BuildContext context) {
+    final singleProduct = Provider.of<Product>(context);
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
         child: GestureDetector(
           onTap: () {
-            Navigator.of(context).pushNamed(ProductDetailScreen.routeName,arguments: id);
+            Navigator.of(context).pushNamed(ProductDetailScreen.routeName,arguments: singleProduct.id);
           },
           child: Image.network(
-            imageUrl,
+            singleProduct.imageUrl,
             fit: BoxFit.cover,
           ),
         ),
         footer: GridTileBar(
           backgroundColor: Colors.black87,
           leading: IconButton(
-            icon: Icon(Icons.favorite),
+            icon: Icon(singleProduct.isFavorite ? Icons.favorite: Icons.favorite_border),
             color: Theme.of(context).accentColor,
-            onPressed: () {},
+            onPressed: () {
+              singleProduct.toggleFavorite();
+            },
           ),
           trailing: IconButton(
             icon: Icon(Icons.shopping_cart),
@@ -35,7 +40,7 @@ class ProductItem extends StatelessWidget {
             onPressed: () {},
           ),
           title: Text(
-            title,
+            singleProduct.title,
             textAlign: TextAlign.center,
           ),
         ),
--- lib/widgets/products_grid.dart
+++ lib/widgets/products_grid.dart
@@ -13,10 +13,13 @@ class ProductsGrid extends StatelessWidget {
 
     return GridView.builder(
         padding: const EdgeInsets.all(10.0),
-        itemBuilder: (ctx, index) => ProductItem(
-          products[index].id,
-          products[index].title,
-          products[index].imageUrl,
+        itemBuilder: (ctx, index) => ChangeNotifierProvider(
+          create: (c) => products[index],
+          child: ProductItem(
+/*             products[index].id,
+            products[index].title,
+            products[index].imageUrl, */
+          ),
         ),
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
